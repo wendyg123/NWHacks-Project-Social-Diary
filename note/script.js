f1b1c84fac4af7e79ad1207d8ddfec37fe44b91e
kelly <hai@dhcp-206-87-98-195.ubcsecure.wireless.ubc.ca>
2023-01-21 20:44:34 -0800

    kelly's part 3

--- note/script.js
+++ note/script.js
@@ -1,8 +1,8 @@
-<body>
+{/* <body>
   <h1>Create Textbox element using JavaScript</h1>
   <script>
     const input = document.createElement("input");
     input.setAttribute("type", "text");
     document.body.appendChild(input);
   </script>
-</body>
+</body> */}
