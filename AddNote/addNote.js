60da86e2907ca98cedaa1918afc8104f21ce96ff
wendyg123 <wendygreening@telus.net>
2023-01-22 00:33:29 -0800

    added face images that switch depending on emotion

--- AddNote/addNote.js
+++ AddNote/addNote.js
@@ -1,4 +1,25 @@
 function assignEmotion(e) {
     emotion_element = document.getElementById("emotion");
     emotion_element.innerHTML = e;
+
+    // figure this out
+    face_element = document.querySelector('img');
+
+    if (e ==='hopeful') {
+        face_element.src = "Images/hopeful.png";
+    } else if (e === "sad") {
+        face_element.src = "Images/sad.png";
+    } else if (e === "lonely") {
+        face_element.src = "Images/lonely.png";
+    } else if (e === "hopeless") {
+        face_element.src = "Images/hopeless.png";
+    } else if (e === "stressed") {
+        face_element.src = "Images/stressed.png";
+    } else if (e === "angry") {
+        face_element.src = "Images/angry.png";
+    } else if (e === "anxious") {
+        face_element.src = "Images/anxious.png";
+    } else if (e === "worried") {
+        face_element.src = "Images/worried.png";
+    }
 }
